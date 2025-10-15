7cc187c931d282baf96d9cf2b743fb47cdb7e294
Omar Fuentes <ofuentes@rccl.com>
2025-10-15 01:20:02 -0600

    Setup nodemon.json and tsconfig.json to work in localhost and be able to use import statement

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import paymentRouter from './routes/payment.routes';
+
 const api = express();
 
 const PORT = process.env.PORT ?? 3000;
 
 api.use(express.json());
-api.use(express.urlencoded())
+api.use(express.urlencoded());
 
-api.get('/', (req, res) => res.send('Response from server.'));
+api.use('/', paymentRouter);
 
 api.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ src/routes/payment.routes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get('/create-order', (req, res) => res.send('Creating payment order!'));
+
+export default router;
