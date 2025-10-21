13d8635995f531d8b5627e91f9442b0369b2bdd1
Omar Fuentes <ofuentes@rccl.com>
2025-10-21 00:13:39 -0600

    Setup nodemon to watch incoming requests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import paymentRouter from './routes/payment.routes';
 
 const app = express();
@@ -7,7 +8,8 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-app.use('/', paymentRouter);
+app.use('/purchase-order', paymentRouter);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
